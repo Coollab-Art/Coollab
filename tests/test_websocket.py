9a0f68ee0ca3d36a4a36e21bf9e1005e45c59e74
Jules Fouchy <jules.fouchy@ntymail.com>
2025-06-18 18:39:23 +0200

    ✅ [Websocket] Cleanup test, start using what could be Coollab's Python API

--- tests/test_websocket.py
+++ tests/test_websocket.py
@@ -1,47 +1,66 @@
 import socket
 import json
-
-# TODO(Websocket) should be moved to Cool. And osc / http tests too probably
-
-HOST = "127.0.0.1"  # or 'localhost'
-PORT = 12345
-
 from time import sleep
+from typing import Optional
+
+# TODO(Websocket) this test should be moved to Cool. And osc / http tests too probably
 
 
-def encode_json(dic: dict):
-    return json.dumps(dic).encode("utf-8") + b"\0"
+class Coollab:
+    _host: str
+    _port: int
+    _s: Optional[socket.socket]
 
+    def __init__(self, host: str = "127.0.0.1", port: int = 12345) -> None:
+        self._host = host
+        self._port = port
+        self._s = None
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    i = 0
-    while True:
-        i += 1
-        if i % 2 == 0:
-            s.sendall(
-                encode_json(
+    def __enter__(self) -> "Coollab":
+        self._s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._s.connect((self._host, self._port))
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        if self._s:
+            self._s.close()
+
+    def _encode_json(self, dic: dict) -> bytes:
+        return json.dumps(dic).encode("utf-8") + b"\0"
+
+    def export_image(self, width: int = 500, height: int = 500) -> None:
+        if self._s:
+            self._s.sendall(
+                self._encode_json(
                     {
                         "command": "ExportImage",
-                        "width": 1024,
-                        "height": 1024,
+                        "width": width,
+                        "height": height,
                         "format": ".png",
                     }
                 )
             )
-        else:
-            s.sendall(
-                encode_json(
+
+    def log(self, title: str, content: str) -> None:
+        if self._s:
+            self._s.sendall(
+                self._encode_json(
                     {
                         "command": "Log",
-                        "title": "Scripting",
-                        "content": f"This is a script! {i}",
+                        "title": title,
+                        "content": content,
                     }
                 )
             )
-        # s.sendall(b"Hello Server\0")
-        sleep(1)
-    # s.sendall(b"Server0")
-    data = s.recv(1024)
 
-print(f"Received: {data.decode()}")
+
+# Usage
+with Coollab() as coollab:
+    i = 0
+    while True:
+        i += 1
+        if i % 2 == 0:
+            coollab.export_image(width=500, height=500)
+        else:
+            coollab.log("Scripting", f"This is a script! {i}")
+        sleep(1)
