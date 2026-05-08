661db551781debb3d3c882103cfea8edeed9dc28
ben.brl <benoitb21@icloud.com>
2026-05-08 17:03:02 +0200

    🌐 [website] Adding compressed GIFs

---
+++ website/content/Tutorials/02-Demo Projects/02-All Projects/00-square-pattern.md
@@ -0,0 +1,14 @@
+---
+title: Square Pattern
+slug: /Demo Projects/square-pattern
+description: Square Pattern demo project shared by Jules Fouchy
+keywords:
+  - Coollab
+  - Coollab demo
+image: /img/demo-project-thumbnails/square-pattern.png
+pagination_prev: null
+---
+
+import DemoProjectContent from '@site/src/components/DemoProjectContent'
+
+<DemoProjectContent title="Square Pattern" />
--- website/src/data/demo-projects.js
+++ website/src/data/demo-projects.js
@@ -6,7 +6,7 @@ const authors = {
 }
 
 const projects = [
-  { title: "Carré Pattern", slug: "carre-pattern", authorId: "jules" },
+  { title: "Square Pattern", slug: "square-pattern", authorId: "jules" },
   { title: "Kaleidoscope", slug: "kaleidoscope", authorId: "jules" },
   {
     title: "Audioreactive Disk",
@@ -20,9 +20,8 @@ const projects = [
   },
   { title: "Colored Squares", slug: "colored-squares", authorId: "jules" },
   { title: "Particules", slug: "particules", authorId: "jules" },
-  { title: "Soleils", slug: "soleils", authorId: "jules" },
+  { title: "Suns", slug: "suns", authorId: "jules" },
   { title: "Space Noise", slug: "space-noise", authorId: "jules" },
-  { title: "Spiral Pattern", slug: "spiral-pattern", authorId: "jules" },
   { title: "Trailer Particles", slug: "trailer-particles", authorId: "jules" },
 ]
 
